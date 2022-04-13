dfc395490d026b5befc59c6e0a36bc4c7488be76
BenignoBatista <101991347+BenignoBatista@users.noreply.github.com>
2022-04-12 21:36:07 -0300

    Resolução atividade online 02

---
+++ ativER_02.js
@@ -0,0 +1,14 @@
+qtdeAlunos = 10;
+
+for (let index = 0; index <= qtdeAlunos; index++) {
+    if (index == 0) {
+        console.log(`Zero: ${index}`);
+    }else if (index % 2 == 0){
+        console.log(`Par: ${index}`);
+    }else{
+        console.log(`Impar: ${index}`);
+    }
+        
+}
+
+
